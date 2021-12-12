903b1f35950703a474fd62297fcc24f13978df43
arpine_ktoyan <arpine@benekiva.com>
2021-12-12 16:04:04 +0400

    feat(core): add index.js file

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const usersRouter = require('./users/routes');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+require('./config/passport')(passport);
+
+app.use(session({
+    name: 'session',
+    secret: 'devsecret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set('view engine', 'pug');
+app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        user: req.session.user,
+        users: req.session.users
+    });
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+app.listen(5050, () => {
+    console.log('Listening on port 5050');
+});
